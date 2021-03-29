e01947b8a1111665cba14ae8a77b2c6a1fdc8bc5
Michal Szwajgier <michalszwajgier@gmail.com>
2021-03-29 19:42:12 +0200

    Fix errors in accordion

--- src/js/script.js
+++ src/js/script.js
@@ -88,7 +88,7 @@
       thisProduct.initOrderForm();
       thisProduct.processOrder();
       
-      console.log('new Product data: ', thisProduct.data); //tu jest to wypisane 
+      //console.log('new Product data: ', thisProduct.data); //tu jest to wypisane 
     }
     renderInMenu() {
       const thisProduct = this;
@@ -122,9 +122,10 @@
         /* prevent default action for event */
         event.preventDefault();
         /* find active product (product that has active class) */     
-        const activeProduct = thisProduct.element.querySelector(classNames.menuProduct.wrapperActive);
+        const activeProduct = document.querySelector('.product.active'); //classNames.menuProduct.wrapperActive
+        console.log('PRODUCT ACTIVE : ', activeProduct);
         /* if there is active product and it's not thisProduct.element, remove class active from it */
-        if (activeProduct && activeProduct !== thisProduct.element) {
+        if (activeProduct !== null && activeProduct !== thisProduct.element) {
           activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
         }
         /* toggle active class on thisProduct.element */
@@ -138,20 +139,17 @@
       thisProduct.form.addEventListener('submit', function(event){
         event.preventDefault();
         thisProduct.processOrder();
-        console.log('1');
       });
       
       for(let input of thisProduct.formInputs){
         input.addEventListener('change', function(){
           thisProduct.processOrder();
-          console.log('2');
         });
       }
       
       thisProduct.cartButton.addEventListener('click', function(event){
         event.preventDefault();
         thisProduct.processOrder();
-        console.log('3');
       });
     }
     processOrder() {
@@ -160,47 +158,47 @@
       // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
       const formData = utils.serializeFormToObject(thisProduct.form);
       /* TO DELETE LATER */
-      console.log('\n----NOWY OBIEKT-----\n\n', thisProduct.id, '\n\nformData', formData); //wszystko co jest w obiektach "params" w wartosciach kluczy w pliku data.js przedstawione w "fajny" sposob dzieki funckji utils.serial..... z pliku functions.js
+      //console.log('\n----NOWY OBIEKT-----\n\n', thisProduct.id, '\n\nformData', formData); //wszystko co jest w obiektach "params" w wartosciach kluczy w pliku data.js przedstawione w "fajny" sposob dzieki funckji utils.serial..... z pliku functions.js
     
       // set price to default price
       let price = thisProduct.data.price; //wszystko co jest w obiektach "price" w wartosciach kluczy w pliku data.js
       /* TO DELETE LATER */
-      console.log('price: ', thisProduct.data.price);
+      //console.log('price: ', thisProduct.data.price);
       // for every category (param)...
       for(let paramId in thisProduct.data.params) { //petla po parametrach (kategoriach)
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
         /* TO DELETE LATER */
-        console.log('params: ', paramId, '\nwartosc klucza params: ', param);
+        //console.log('params: ', paramId, '\nwartosc klucza params: ', param);
     
         // for every option in this category
         for(let optionId in param.options) { //schodzimy jeden level glebiej w drzewie do opcji kazdego parametru
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId]; 
           
-          console.log('params -> option (nazwa kolejnej podtablicy - nazwa klucza): ', optionId, '\nwartosci klucza opcji: ', option); 
+          //console.log('params -> option (nazwa kolejnej podtablicy - nazwa klucza): ', optionId, '\nwartosci klucza opcji: ', option); 
 
-          console.log('-----------------------------------\nformData[paramId]: ', formData[paramId], '\n----------------------------------');
+          //console.log('-----------------------------------\nformData[paramId]: ', formData[paramId], '\n----------------------------------');
           
           const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
           if(optionImage) {
-            console.log('IMAGE FOUND! : ', optionImage);
+            //console.log('IMAGE FOUND! : ', optionImage);
             if(formData[paramId] && formData[paramId].includes(optionId)) {
-              console.log('SPRAWDZAM CZY IMAGE FOUND');
+              //console.log('SPRAWDZAM CZY IMAGE FOUND');
               // check if the option is default
               if(!optionId) {
-                console.log('IMAGE == FALSE');
+                //console.log('IMAGE == FALSE');
                 // remove class activ (visibility)
                 optionImage.classList.remove(classNames.menuProduct.imageVisible); 
               } else {
-                console.log('IMAGE == TRUE');
+                //console.log('IMAGE == TRUE');
                 // add class activ (visibility)
                 optionImage.classList.add(classNames.menuProduct.imageVisible);             
               }
             } else { 
               // check if the option is not default
               if(optionId) {
-                console.log('IMAGE == FALSE');
+                //console.log('IMAGE == FALSE');
                 // remove class activ (visibility)
                 optionImage.classList.remove(classNames.menuProduct.imageVisible);
               }
@@ -210,24 +208,24 @@
 
           // check if there is param with a name of paramId in formData and if it includes optionId
           if(formData[paramId] && formData[paramId].includes(optionId)) {
-            console.log('SPRAWDZAM CZY TRUE');
+            //console.log('SPRAWDZAM CZY TRUE');
             // check if the option is default
             if(!optionId) {
-              console.log('DEFAULT == FALSE');
+              //console.log('DEFAULT == FALSE');
             } else {
-              console.log('DEFAULT == TRUE');
-              console.log(option.price);              
+              //console.log('DEFAULT == TRUE');
+              //console.log(option.price);              
               // reduce price variable
               price += option.price;
-              console.log('price after action: ', price);
+              //console.log('price after action: ', price);
             }
           } else { 
             // check if the option is not default
             if(optionId) {
-              console.log('DEFAULT == FALSE');
+              //console.log('DEFAULT == FALSE');
             }
           }
-          console.log('888888888888888888888888888888888888888888888888888888888888888888888888888888'); 
+          //console.log('888888888888888888888888888888888888888888888888888888888888888888888888888888'); 
         }
       }
     
