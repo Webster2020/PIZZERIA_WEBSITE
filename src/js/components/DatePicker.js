90a633660a08a6ec1e88c5b1cb2e3bb371c98058
Michal Szwajgier <michalszwajgier@gmail.com>
2021-04-21 17:23:25 +0200

    Fix bug in dataPicker

--- src/js/components/DatePicker.js
+++ src/js/components/DatePicker.js
@@ -1,4 +1,4 @@
-import BaseWidget from '../Components/BaseWidget.js';
+import BaseWidget from '../components/BaseWidget.js';
 import utils from '../utils.js';
 import {select, settings} from '../settings.js';
 
